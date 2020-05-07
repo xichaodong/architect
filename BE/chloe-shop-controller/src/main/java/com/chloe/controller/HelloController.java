364ba0c704b3e1888fb3d5e8f54243d4771e3280
tristeza <1768205721@qq.com>
2020-05-07 11:48:05 +0800

    20200506-day1

---
+++ BE/chloe-shop-controller/src/main/java/com/chloe/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.chloe.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String test() {
+        return "111";
+    }
+}
