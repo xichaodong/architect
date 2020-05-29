bfe755d7dba89fcbbb4a937a58319f5a18800a14
tristeza <1768205721@qq.com>
2020-05-29 13:46:52 +0800

    20200528-day23

---
+++ 1-5周 单体架构 7周redis/BE/chloe-shop-controller/src/main/java/com/chloe/controller/IndexController.java
@@ -0,0 +1,91 @@
+package com.chloe.controller;
+
+import com.chloe.common.enums.BooleanEnum;
+import com.chloe.common.utils.JsonResult;
+import com.chloe.common.utils.JsonUtils;
+import com.chloe.common.utils.RedisOperator;
+import com.chloe.model.pojo.Carousel;
+import com.chloe.model.pojo.Category;
+import com.chloe.model.vo.SubCategoryVO;
+import com.chloe.model.vo.SuggestItemVO;
+import com.chloe.service.CarouselService;
+import com.chloe.service.CategoryService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Objects;
+
+@Api(value = "首页", tags = {"首页展示的相关接口"})
+@RestController
+@RequestMapping("index")
+public class IndexController {
+    private static final String CAROUSEL_CACHE = "CAROUSEL";
+
+    @Resource
+    private CarouselService carouselService;
+    @Resource
+    private CategoryService categoryService;
+    @Resource
+    private RedisOperator redisOperator;
+
+    @ApiOperation(value = "获取首页轮播图列表", notes = "获取首页轮播图列表", httpMethod = "GET")
+    @GetMapping("carousel")
+    public JsonResult carousel() {
+        String carouselCache = redisOperator.get(CAROUSEL_CACHE);
+
+        if (StringUtils.isBlank(carouselCache)) {
+            List<Carousel> carousels = carouselService.queryAll(BooleanEnum.TRUE.type);
+            redisOperator.set(CAROUSEL_CACHE, JsonUtils.objectToJson(carousels));
+
+            return JsonResult.ok(carousels);
+        }
+
+        return JsonResult.ok(JsonUtils.jsonToList(carouselCache, Carousel.class));
+    }
+
+    @ApiOperation(value = "获取首页大分类", notes = "获取首页大分类", httpMethod = "GET")
+    @GetMapping("cats")
+    public JsonResult rootCats() {
+        List<Category> categories = categoryService.queryAllRootCategory();
+        return JsonResult.ok(categories);
+    }
+
+    @ApiOperation(value = "获取大分类的子分类", notes = "获取大分类的子分类", httpMethod = "GET")
+    @GetMapping("subCat/{rootCatId}")
+    public JsonResult rootCats(
+            @ApiParam(name = "rootCatId", value = "一级分类id", required = true, example = "1")
+            @PathVariable Integer rootCatId) {
+
+        if (Objects.isNull(rootCatId)) {
+            return JsonResult.errorMsg("分类id不能为空");
+        }
+
+        List<SubCategoryVO> subCategories = categoryService.querySubCategoryByRootId(rootCatId);
+
+        return JsonResult.ok(subCategories);
+    }
+
+    @ApiOperation(value = "查询每个一级分类下最新的推荐商品", notes = "查询每个一级分类下最新的推荐商品", httpMethod = "GET")
+    @GetMapping("sixNewItems/{rootCatId}")
+    public JsonResult suggestItems(
+            @ApiParam(name = "rootCatId", value = "一级分类id", required = true, example = "1")
+            @PathVariable Integer rootCatId) {
+
+        if (Objects.isNull(rootCatId)) {
+            return JsonResult.errorMsg("分类id不能为空");
+        }
+
+        List<SuggestItemVO> suggestItemInfos = categoryService.querySuggestItemInfo(rootCatId);
+
+        return JsonResult.ok(suggestItemInfos);
+    }
+}
