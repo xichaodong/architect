ccd39f5658dc0878770257a9536ec71803f95224
tristeza <1768205721@qq.com>
2021-03-03 21:41:08 +0800

    20200827-day113

---
+++ 21周 配置中心/config-client/src/main/java/com/tristeza/config/client/ConfigClientApplication.java
@@ -0,0 +1,17 @@
+package com.tristeza.config.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author chaodong.xi
+ * @date 2021/3/3 11:23 上午
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ConfigClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+}
---
+++ 21周 配置中心/config-server/src/main/java/com/tristeza/config/server/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package com.tristeza.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author chaodong.xi
+ * @date 2021/3/3 11:23 上午
+ */
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
